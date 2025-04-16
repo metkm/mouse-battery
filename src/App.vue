7f6eed3d946e61ba3f125d6c426ad7ee334c6a63
metkm <metinkorkmaz417@gmail.com>
2025-04-16 20:55:33 +0300

    hm

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { getDevice, getUsbCrc } from './utils';
+import { ref } from "vue";
+import { getDevice, getEepromBuffer, getUsbCrc, setArrayEeprom, setByteEeprom } from "./utils";
+import handle from "./handle.js";
 
-const battery = ref(0)
+const battery = ref(0);
 
 const handleClick = async () => {
-  const device = await getDevice()
+  const device = await getDevice();
   if (!device) return;
 
   if (!device.opened) {
@@ -14,19 +15,31 @@ const handleClick = async () => {
 
   device.oninputreport = (event) => {
     const data = new Uint8Array(event.data.buffer);
+    console.log(data);
+
     const dataCommand = data[0];
+    const flashData = [];
 
     if (dataCommand === 4) {
-      battery.value = data[5]
+      battery.value = data[5];
+    } else if (dataCommand === 8) {
+      const addr = (data[2] << 8) + data[3];
+      const len = data[4];
+
+      for (var i = 0; i < len; i++) {
+        flashData[addr + i] = data[5 + i];
+      }
+
+      console.log(flashData)
     }
   };
 
   let data = Uint8Array.of(
-    0x04,
+    0x08,
     0x00,
-    0x00,
-    0x00,
-    0x00,
+    0x04 >> 8,
+    0x04 & 0xff,
+    2,
     0x00,
     0x00,
     0x00,
@@ -40,10 +53,10 @@ const handleClick = async () => {
     0xef
   );
 
-  const crc = getUsbCrc(data)
-  data[15] = crc  - 0x08;
+  const crc = getUsbCrc(data);
+  data[15] = crc - 0x08;
 
-  await device.sendReport(0x08, data)
+  await device.sendReport(0x08, data);
 };
 </script>
 
--- src/utils.ts
+++ src/utils.ts
@@ -17,15 +17,64 @@ export const getDevice = async () => {
   const device =
     devices.length > 0
       ? devices.find((device) => {
-          return device.collections.find((c) => {
-            return (
-              c.inputReports!.length === 1 &&
-              c.outputReports!.length === 1 &&
-              c.outputReports![0].reportId === 0x08
-            );
-          });
-        })
+        return device.collections.find((c) => {
+          return (
+            c.inputReports!.length === 1 &&
+            c.outputReports!.length === 1 &&
+            c.outputReports![0].reportId === 0x08
+          );
+        });
+      })
       : devices[0];
 
   return device
+}
+
+export const getEepromBuffer = async (address: number, length: number) => {
+  let data = Uint8Array.of(0x08, 0x00, address >> 8, address & 0xFF, length, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xef);
+
+  return data
+}
+
+
+export const setByteEeprom = (address: number, value: number) => {
+  let data = Uint8Array.of(0x07, 0x00, address >> 8, address & 0xFF, 0x02, 0x08, 0x4d,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xef);
+
+  data[5] = value
+  // data[6] = 0x55 - value;
+
+  return data
+}
+
+export const setArrayEeprom = (address: number, value: number[]) => {
+  let data = Uint8Array.of(0x07, 0x00, address >> 8, address & 0xFF, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xef);
+
+  var cnt = (value.length % 10);
+  cnt = (cnt > 0) ? (Math.floor(value.length / 10) + 1) : Math.floor(value.length / 10);
+  console.log("setArrayEeprom:", cnt, value.length, value);
+  for (var i = 0; i < cnt; i++) {
+    var add = (address + i * 10);
+    var len = ((((i + 1) * 10) > value.length) ? (value.length - (i * 10)) : 10);
+
+    data[0] = 0x07;
+    data[1] = 0x00;
+    console.log("setArrayEeprom len", len);
+    data[2] = add >> 8;
+    data[3] = add & 0xFF;
+    data[4] = len;
+    for (var j = 0; j < 10; j++) {
+      if (j < len)
+        data[5 + j] = value[j + i * 10];
+      else
+        data[5 + j] = 0;
+    }
+
+    // data[15] = getUsbCrc(data) - ReportId;
+    // await write(data);
+  }
+
+  return data
 }
